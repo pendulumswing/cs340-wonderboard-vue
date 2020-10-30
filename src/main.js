fe0321522ab4c3389514388ca00f6ea174e25a14
pendulumswing <jason@pendulumswingmedia.com>
2020-10-30 10:28:30 -0700

    Add Vuetify

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 import './filters'
 
 Vue.config.productionTip = false
+// Vue.use(Vuetify) //Add any theme modifications here
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <v-card>
+      <v-card-title>Users</v-card-title>
+      <v-card-text>
+        <div v-for="user in users">
+          <span> Name: {{ user.name }} </span>
+          <span>  Email: {{ user.email }} </span>
+        </div>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -12,6 +21,15 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+
+  data () {
+    return {
+      users: [
+        {name: 'Jason', email: 'jb@email.com'},
+        {name: 'Peter', email: 'pw@email.com'},
+      ]
+    }
   }
 }
 </script>
