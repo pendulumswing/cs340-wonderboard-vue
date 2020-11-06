42bc3c57d649e7e7d1e3ca325fecbe0ed1a0a077
pendulumswing <jason@pendulumswingmedia.com>
2020-11-06 10:46:11 -0700

    Add colors to Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    colors: [
+      { name: 'Red', color: 'red lighten-3' },
+      { name: 'Blue', color: 'blue lighten-3' },
+      { name: 'Green', color: 'green lighten-3' },
+      { name: 'Orange', color: 'orange lighten-3' },
+      { name: 'Purple', color: 'purple lighten-3' },
+      { name: 'Yellow', color: 'yellow lighten-3' },
+      { name: 'Grey', color: 'grey lighten-3' },
+      { name: 'White', color: 'white' },
+      { name: 'Dark Blue', color: 'blue' },
+    ]
   },
   mutations: {
 
