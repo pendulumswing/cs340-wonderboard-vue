6b2d6b55e90dfcc3c7ab8f7cb99c616aacb69bb7
pendulumswing <jason@pendulumswingmedia.com>
2020-11-20 21:23:35 -0700

    Add axios to project with interceptors

--- src/main.js
+++ src/main.js
@@ -3,8 +3,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-
+import axios from 'axios'
 import './filters'
+
+axios.defaults.baseURL = '/api/'
+// axios.defaults.headers.common['Authorization'] = 'password_key'
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+// Request Interceptor
+axios.interceptors.request.use(config => {
+  // console.log('Request Interceptor', config)
+  // config.headers['Authorization'] = 'Fake Token'
+  return config
+})
+
+// Response Interceptor to handle and log errors
+axios.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  // Handle Error
+  console.log('Response Interceptor', error)
+  return Promise.reject(error)
+})
+
+// let $axios = axios.defaults ({
+//   baseURL: '/api/',
+//   timeout: 5000,
+//   headers: { 'Content-Type': 'application/json' }
+// })
 
 Vue.config.productionTip = false
 // Vue.use(Vuetify) //Add any theme modifications here
