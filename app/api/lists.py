5c9066595ee97bdde39ddf20fe93953ab7702087
pendulumswing <jason@pendulumswingmedia.com>
2020-12-04 11:12:24 -0700

    Update list route to accept NULL values for creator id

--- app/api/lists.py
+++ app/api/lists.py
@@ -1,34 +1,14 @@
 from .resources import *
 
 
-def update_one_list(table, data, id):
-
-    conn = get_connection()
-    query = (f"""UPDATE {table}
-            SET board = '{data['board']}',
-            name = '{data['name']}',
-            index = '{data['index']}',
-            creator = '{data['creator']}',
-            color = '{data['color']}'
-            WHERE id={id};""")
-    query_params = (id,)
-
-    with conn.cursor() as cur:
-        cur.execute(query)
-        conn.commit()
-        result = find_one('lists', id)
-
-    return result, 200
-
-
 @api_rest.route('/lists')
 class List(Resource):
 
-    # finding all lists
+    # Find all lists
     def get(self):
         return find_all('lists'), 200
 
-    # insert list
+    # Insert list
     def post(self):
         data = request.json
         conn = get_connection()
@@ -48,22 +28,44 @@ class List(Resource):
         return result, 201
 
 
-
 @api_rest.route('/lists/<int:resource_id>')
 class Lists(Resource):
 
-    # finding all lists based on boardId
+    # Find list
     def get(self, resource_id):
         # print(resource_id)
         return find_one('lists', resource_id), 200
 
-    # delete board based on listId
+    # Delete list
     def delete(self, resource_id):
         # print(resource_id)
         return delete_one('lists', resource_id)
 
 
-    #update board based on boardId
+    # Update list
     def put(self, resource_id):
         data = request.json
         return update_one_list('lists', data, resource_id)
+
+
+def update_one_list(table, data, id):
+    # Check for NULL value for Creator
+    if(data['creator'] == None):
+        data['creator'] = 'NULL'
+
+    conn = get_connection()
+    query = (f"""UPDATE {table}
+            SET board = {data['board']},
+            name = '{data['name']}',
+            index = {data['index']},
+            creator = {data['creator']},
+            color = '{data['color']}'
+            WHERE id={id};""")
+    query_params = (id,)
+
+    with conn.cursor() as cur:
+        cur.execute(query)
+        conn.commit()
+        result = find_one('lists', id)
+
+    return result, 200
