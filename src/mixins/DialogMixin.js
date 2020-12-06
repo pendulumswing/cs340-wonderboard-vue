f291e86b402f714f579a8f1eda03364058bb2be7
pendulumswing <jason@pendulumswingmedia.com>
2020-12-05 20:36:43 -0700

    Cleanup Dialog Mixin

--- src/mixins/DialogMixin.js
+++ src/mixins/DialogMixin.js
@@ -1,16 +1,3 @@
-import { mapState } from 'vuex'
-
-function getErrorMessage (error) {
-  const keys = Object.keys(error)
-  const key = keys[0]
-
-  if (key) {
-    return `${key}: ${error[key][0]}`
-  }
-
-  return 'Unknown error'
-}
-
 export default {
   props: {
     noEnter: {
@@ -42,48 +29,19 @@ export default {
 
   watch: {
     _request: function (newModel, oldModel) {
-      // if (newModel.state === PayloadStates.RESOLVED ||
-      //   newModel.state === PayloadStates.DELETING ||
-      //   newModel.state === PayloadStates.DELETED) {
-      //   this.onSuccess()
-      //   this.onClose()
-      // }
-      // if (
-      //   newModel.state === PayloadStates.ERROR_CREATING ||
-      //   newModel.state === PayloadStates.ERROR_UPDATING ||
-      //   newModel.state === PayloadStates.ERROR_DELETING
-      // ) {
-      //   this.error = true
-      //   this.errorMessage = getErrorMessage(newModel.error)
-      //   this.onError(newModel.error, this.errorMessage)
-      // }
+
     }
   },
 
   mounted () {
-
     // Submit form on pressing 'Enter'
     // SOURCE: https://github.com/vuetifyjs/vuetify/issues/1545
     if (!this.noEnter) {
       window.addEventListener('keyup', this.onEnter)
     }
 
+    // Close dialog on pressing 'Esc'
     window.addEventListener('keyup', this.onEsc)
-
-    // if (!this.noEnter) {
-    //   window.addEventListener('keyup', function (event) {
-    //     if (event.key === 'Enter') {
-    //       that.onSubmit()
-    //     }
-    //   })
-    // }
-    //
-    // // Submit form on pressing 'Escape'
-    // window.addEventListener('keyup', function (event) {
-    //   if (event.key === 'Escape') {
-    //     that.onClose()
-    //   }
-    // })
   },
 
   methods: {
@@ -94,31 +52,27 @@ export default {
         this.$emit('close')
       }, 300)
     },
+
     getRequest (request) {
 
     },
+
     onSuccess () {
 
     },
+
+    // eslint-disable-next-line handle-callback-err
     onError (error, errorMessage) {
 
     },
+
     onSubmit () {
 
     },
-    // validate () {
-    //   this.$refs.form.validate()
-    // },
-    // reset () {
-    //   this.$refs.form.reset()
-    // },
-    // resetValidation () {
-    //   this.$refs.form.resetValidation()
-    // },
+
     onEnter (event) {
       const that = this
       if (event.key === 'Enter') {
-        console.log('onEnter called')
         that.onSubmit()
       }
     },
@@ -126,13 +80,11 @@ export default {
     onEsc (event) {
       const that = this
       if (event.key === 'Escape') {
-        console.log('onEsc called')
         that.onClose()
       }
     },
 
     removeEventListeners () {
-      console.log('removing onEnter listener')
       window.removeEventListener('keyup', this.onEnter)
       window.removeEventListener('keyup', this.onEsc)
     }
