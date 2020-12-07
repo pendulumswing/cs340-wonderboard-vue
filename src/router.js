515f92cb6d4d534d473aa256819632d1052b4571
pendulumswing <jason@pendulumswingmedia.com>
2020-12-06 18:55:16 -0700

    Switch from Router to VueRouter

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+// import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Home from './pages/home/Layout'
 import Api from './pages/api/Layout'
 import User from './pages/user/Layout'
@@ -7,9 +8,10 @@ import Board from './pages/user-board/Layout'
 import Dev from './pages/dev/Layout'
 import NotFound from './components/NotFound'
 
-Vue.use(Router)
+// Vue.use(VueRouter.createRouter)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', name: 'home', component: Home },
